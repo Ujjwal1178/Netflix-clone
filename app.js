c918d5df4ae3277f1ba688d476577957fde7c6a0
Ujjwal <ujjawalthakur687@gmail.com>
2024-03-19 20:03:31 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// let changeval = document.querySelector('.txt');
+// function change(){
+//     changeval.classList.toggle('txt1');
+// }
+
+
+
+let changeval1 = document.querySelector('#texta1');
+
+function change1(){
+    changeval1.classList.toggle('visible');
+    document.getElementById("plus1").setAttribute('transform','rotate(45deg');
+    
+}
+
+
+let changeval2 = document.querySelector('#texta2');
+function change2(){
+    changeval2.classList.toggle('visible');
+}
+
+
+let changeval3 = document.querySelector('#texta3');
+function change3(){
+    changeval3.classList.toggle('visible');
+}
+
+
+let changeval4 = document.querySelector('#texta4');
+function change4(){
+    changeval4.classList.toggle('visible');
+}
+
+
+let changeval5 = document.querySelector('#texta5');
+function change5(){
+    changeval5.classList.toggle('visible');
+}
+
+
+let changeval6 = document.querySelector('#texta6');
+function change6(){
+    changeval6.classList.toggle('visible');
+    
+}
